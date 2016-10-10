d0e9a4cf3eb0587484ab8db62d6a3fcb8353c983
Kshitij Awadhiya <kshitij@browserstack.com>
2016-10-10 13:28:53 +0530

    Initial Setup

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var app = express();
+var port = 3002;
+var path = require('path');
+
+app.set('view engine', 'html');
+app.engine('html', require('hogan-express'));
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/line_chart', function(req, res) {
+    res.locals = { size : { x: 700, y: 700 }};
+    res.render('line_chart');  
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  
+  console.log("--- Running on " + port + " ---");
+});
